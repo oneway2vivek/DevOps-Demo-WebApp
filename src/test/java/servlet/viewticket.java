6128d7fac8b85f26fab2dd83341223b05aed3099
oneway2vivek <oneway2vivek@gmail.com>
2021-02-08 23:19:48 -0500

    Update viewticket.java

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.219.121.46:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.225.32.120:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
