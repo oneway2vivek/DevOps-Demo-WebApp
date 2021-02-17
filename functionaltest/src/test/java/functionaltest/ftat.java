79ef580737eefe0a58e5013557d7817aea0b70d9
oneway2vivek <oneway2vivek@gmail.com>
2021-02-17 09:37:08 -0500

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://18.191.195.76:8080/QAWebapp/";
-	public String URL1 = "http://18.191.195.76:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://3.138.134.218:8080/QAWebapp/";
+	public String URL1 = "http://3.138.134.218:8080/QAWebapp/avncreatepage.jsp";
 	 
 	@Test(priority = 0)
 	public void logintest() {
